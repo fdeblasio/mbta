4fd85ab3e3b75c28e8a6bf375b146f292897180e
Frank <fdeblasio1004@gmail.com>
2025-09-01 10:23:25 -0400

    Added vehicles info and new function

--- mbta.py
+++ mbta.py
@@ -1,6 +1,11 @@
 import json
 import requests
 
+def getJson(api):
+    with open(api + ".json", "w+") as stops:
+        stops.seek(0)
+        stops.write(requests.get("https://api-v3.mbta.com/" + api).text)
+
 ### Stops
 
 ##with open("stops.json", "w+") as stops:
@@ -24,22 +29,25 @@ import requests
 #                    stops.write("  " + field + ": " + str(stop[field]) + "\n")
 ### Lines
 
-api = "lines"
-#with open(api + ".json", "w+") as stops:
-#    stops.seek(0)
-#    stops.write(requests.get("https://api-v3.mbta.com/" + api).text)
+#api = "lines"
+##with open(api + ".json", "w+") as stops:
+##    stops.seek(0)
+##    stops.write(requests.get("https://api-v3.mbta.com/" + api).text)
+#
+#data = ""
+#
+#with open(api + ".json", "r+") as f:
+#    data = json.load(f)
+#
+#data = data["data"]
+#
+#with open(api + ".txt", "w+") as lines:
+#    lines.seek(0)
+#    for line in data:
+#        lines.write(line["attributes"]["long_name"] + "\n")
+#        for field in line:
+#            if field != "type":
+#                lines.write("  " + field + ": " + str(line[field]) + "\n")
 
-data = ""
-
-with open(api + ".json", "r+") as f:
-    data = json.load(f)
-
-data = data["data"]
-
-with open(api + ".txt", "w+") as lines:
-    lines.seek(0)
-    for line in data:
-        lines.write(line["attributes"]["long_name"] + "\n")
-        for field in line:
-            if field != "type":
-                lines.write("  " + field + ": " + str(line[field]) + "\n")
+### Vehicles
+#getJson("vehicles")
