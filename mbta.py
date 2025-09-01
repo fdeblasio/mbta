c07958d57bf22c87364ded29a78c60192eb75b4e
Frank <fdeblasio1004@gmail.com>
2025-09-01 10:22:50 -0400

    Added lines info

--- mbta.py
+++ mbta.py
@@ -1,22 +1,45 @@
 import json
 import requests
 
-#with open("stops.json", "w+") as stops:
+### Stops
+
+##with open("stops.json", "w+") as stops:
+##    stops.seek(0)
+##    stops.write(requests.get("https://api-v3.mbta.com/stops").text)
+#
+#data = ""
+#
+#with open("stops.json", "r+") as f:
+#    data = json.load(f)
+#
+#data = data["data"]
+#
+#with open("stops.txt", "w+") as stops:
 #    stops.seek(0)
-#    stops.write(requests.get("https://api-v3.mbta.com/stops").text)
+#    for stop in data:
+#        if stop["attributes"]["vehicle_type"] != 3:
+#            stops.write(stop["attributes"]["name"] + "\n")
+#            for field in stop:
+#                if field != "type":
+#                    stops.write("  " + field + ": " + str(stop[field]) + "\n")
+### Lines
+
+api = "lines"
+#with open(api + ".json", "w+") as stops:
+#    stops.seek(0)
+#    stops.write(requests.get("https://api-v3.mbta.com/" + api).text)
 
 data = ""
 
-with open("stops.json", "r+") as f:
+with open(api + ".json", "r+") as f:
     data = json.load(f)
 
 data = data["data"]
 
-with open("stops.txt", "w+") as stops:
-    stops.seek(0)
-    for stop in data:
-        if stop["attributes"]["vehicle_type"] != 3:
-            stops.write(stop["attributes"]["name"] + "\n")
-            for field in stop:
-                if field != "type":
-                    stops.write("  " + field + ": " + str(stop[field]) + "\n")
+with open(api + ".txt", "w+") as lines:
+    lines.seek(0)
+    for line in data:
+        lines.write(line["attributes"]["long_name"] + "\n")
+        for field in line:
+            if field != "type":
+                lines.write("  " + field + ": " + str(line[field]) + "\n")
