57de2f3e6ec3cd09aee9b88fa4ab7b20a4764a1d
Frank <fdeblasio1004@gmail.com>
2025-09-01 11:56:17 -0400

    More vehicle updates

--- mbta.py
+++ mbta.py
@@ -45,21 +45,19 @@ api = "vehicles"
 vehicleData = getData(api)
 localBuses = ["132"]
 
-greenLine = {
-    36: "Type 7",
-    37: "Type 7",
-    38: "Type 8",
-    39: "Type 9"
-}
-
-redLine = {
-    15: "Pullman-Standard",
-    16: "Pullman-Standard",
-    17: "UTDC",
-    18: "Bombardier",
-    19: "CRRC",
-    20: "CRRC",
-    21: "CRRC"
+redGreenLine = {
+    "15": "Pullman-Standard",
+    "16": "Pullman-Standard",
+    "17": "UTDC",
+    "18": "Bombardier",
+    "19": "CRRC",
+    "20": "CRRC",
+    "21": "CRRC",
+    "35": "US Standard Light Rail Vehicle",
+    "36": "Type 7",
+    "37": "Type 7",
+    "38": "Type 8",
+    "39": "Type 9"
 }
 
 with open(api + ".txt", "w+") as vehicles:
@@ -69,18 +67,35 @@ with open(api + ".txt", "w+") as vehicles:
 
         isBus = routeId[0].isdigit() and routeId not in localBuses
         isCR = routeId.startswith("CR")
+        isShuttleGeneric = routeId.startswith("Shuttle-Generic")
 
-        if not isBus and not isCR:
-            writeToFile(vehicles, routeId + " " + vehicle["id"])
+        if not isBus and not isCR and not isShuttleGeneric:
+            if routeId == "Red" or routeId.startswith("Green"):
+                writeToFile(vehicles, routeId + " " + redGreenLine[vehicle["attributes"]["label"][:2]] + " " + vehicle["id"])
+            else:
+                writeToFile(vehicles, routeId + " " + vehicle["id"])
             for field in vehicle:
-                if field == "attributes" or field == "relationships":
-                    vehicles.write("  " + field + ":\n")
-                    for value in vehicle[field]:
-                        if value == "carriages":
-                            writeToFile(vehicles, "    " + value + ":")
-                            for carriage in vehicle[field][value]:
+                if field == "attributes":
+                    writeToFile(vehicles, "  " + field + ":")
+                    for attribute in vehicle[field]:
+                        attributeValue = vehicle[field][attribute]
+                        if attribute == "carriages":
+                            writeToFile(vehicles, "    " + attribute + ":")
+                            for carriage in attributeValue:
                                 writeToFile(vehicles, "      " + str(carriage))
-                        elif value != "revenue":
-                            writeToFile(vehicles, "    " + value + ": " + str(vehicle[field][value]))
-                elif field not in ["type", "links", "id"]:
-                    writeToFile(vehicles, "  " + field + ": " + str(vehicle[field]))
+                        elif attribute == "direction_id":
+                            direction = "South" if attributeValue == 0 else "North"
+                            writeToFile(vehicles, "    " + attribute + ": " + str(attributeValue) + " (" + direction + ")")
+                        elif attribute != "revenue":
+                            writeToFile(vehicles, "    " + attribute + ": " + str(attributeValue))
+                elif field == "relationships":
+                    writeToFile(vehicles, "  " + field + ":")
+                    for relationship in vehicle[field]:
+                        if vehicle[field][relationship]["data"] is not None:
+                            relationshipValue = vehicle[field][relationship]["data"]["id"]
+                        else:
+                            relationshipValue = "N/A"
+                        if relationship != "route":
+                            writeToFile(vehicles, "    " + relationship + ": " + str(relationshipValue))
+                #elif field not in ["type", "links", "id"]:
+                #    writeToFile(vehicles, "  " + field + ": " + str(vehicle[field]))
